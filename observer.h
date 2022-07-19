93ff1fbbea38ff03604df4b723f6fdf3d63976a1
ay7xu <ay7xu@uwaterloo.ca>
2022-07-18 22:55:26 -0400

    subject.cc

--- observer.h
+++ observer.h
@@ -1,9 +1,8 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
 #include <unordered_set>
-class Subject;
 enum class Resource {Brick, Energy, Glass, Heat, Wifi, Park};
-
+class Subject;
 class Observer {
     public: 
         virtual void notify(Subject &s) = 0;
@@ -13,15 +12,10 @@ class Observer {
 class Subject {
     std::unordered_set<Observer*> observers;
     public:
-        void notify_observers() {
-            for(auto x : observers) x->notify(*this);
-        };
-        void attach( Observer *o) {
-            observers.insert(o);
-        }
-        void detach( Observer *o) {
-            observers.erase(o);
-        }
+        void notify_observers();
+        void attach( Observer *o);
+        void detach( Observer *o);
         virtual ~Subject() = default;
 };
+
 #endif
