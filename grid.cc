6ec651ae87749a881298d8a40342e6b724f43eeb
exydu <exydu@uwaterloo.ca>
2022-07-19 17:18:12 -0400

    corrected bug in validroad

--- grid.cc
+++ grid.cc
@@ -28,27 +28,27 @@ void Grid::node_owner_init(std::unordered_map<size_t, Building *> &map) {
 
 void Grid::edge_ends_init(std::unordered_map<size_t, std::pair<size_t, size_t>> &map) {
     map[0] = std::make_pair(0, 1);
-    map[1] = std::make_pair(0,3);
-    map[2] = std::make_pair(1,4);
-    map[3] = std::make_pair(2,3);
-    map[4] = std::make_pair(4,5);
-    map[5] = std::make_pair(2,7);
-    map[6] = std::make_pair(3,8);
-    map[7] = std::make_pair(4,9);
-    map[8] = std::make_pair(5,10);
-    map[9] = std::make_pair(6,7);
-    map[10] = std::make_pair(8,9);
-    map[11] = std::make_pair(10,11);
+    map[1] = std::make_pair(0, 3);
+    map[2] = std::make_pair(1, 4);
+    map[3] = std::make_pair(2, 3);
+    map[4] = std::make_pair(4, 5);
+    map[5] = std::make_pair(2, 7);
+    map[6] = std::make_pair(3, 8);
+    map[7] = std::make_pair(4, 9);
+    map[8] = std::make_pair(5, 10);
+    map[9] = std::make_pair(6, 7);
+    map[10] = std::make_pair(8, 9);
+    map[11] = std::make_pair(10, 11);
     for (size_t i = 12; i <= 17; i++) {
-        map[i] = std::make_pair(i-6, i);
+        map[i] = std::make_pair(i - 6, i);
     }
-    map[18] = std::make_pair(13,14);
-    map[19] = std::make_pair(15,16);
+    map[18] = std::make_pair(13, 14);
+    map[19] = std::make_pair(15, 16);
     for (size_t i = 20; i <= 25; i++) {
         map[i] = std::make_pair(i - 8, i - 2);
     }
-    map[26] = std::make_pair(18,19);
-    map[27] = std::make_pair(20,21);
+    map[26] = std::make_pair(18, 19);
+    map[27] = std::make_pair(20, 21);
     map[28] = std::make_pair(22, 23);
     for (size_t i = 29; i <= 34; i++) {
         map[i] = std::make_pair(i - 11, i - 5);
@@ -64,84 +64,83 @@ void Grid::edge_ends_init(std::unordered_map<size_t, std::pair<size_t, size_t>> 
     for (size_t i = 46; i <= 51; i++) {
         map[i] = std::make_pair(i - 16, i - 10);
     }
-    map[52] = std::make_pair(37,38);
-    map[53] = std::make_pair(39,40);
+    map[52] = std::make_pair(37, 38);
+    map[53] = std::make_pair(39, 40);
     for (size_t i = 54; i <= 59; i++) {
         map[i] = std::make_pair(i - 18, i - 12);
     }
     map[60] = std::make_pair(42, 43);
-    map[61] = std::make_pair(44,45);
+    map[61] = std::make_pair(44, 45);
     map[62] = std::make_pair(46, 47);
     for (size_t i = 63; i <= 66; i++) {
         map[i] = std::make_pair(i - 20, i - 15);
     }
     map[67] = std::make_pair(48, 49);
-    map[68] = std::make_pair(50,51);
-    map[69] = std::make_pair(49,52);
-    map[70] = std::make_pair(50,53);
-    map[71] = std::make_pair(52,53);
-
+    map[68] = std::make_pair(50, 51);
+    map[69] = std::make_pair(49, 52);
+    map[70] = std::make_pair(50, 53);
+    map[71] = std::make_pair(52, 53);
 }
 
 void Grid::adjacent_edges_init(std::unordered_map<size_t, std::vector<size_t>> &map) {
-    map[0] = {0,1};
-    map[1] = {0,2};
-    map[2] = {3,5};
-    map[3] = {1,3,6};
-    map[4] = {2,4,7};
-    map[5] = {4,8};
-    map[6] = {9,12};
-    map[7] = {5,9,13};
-    map[8] = {6,10,14};
-    map[9] = {7,10,15};
-    map[10] = {8,11,16};
-    map[11] = {11,17};
-    map[12] = {12,20};
-    map[13] = {13,18,21};
-    map[14] = {14,18,22};
-    map[15] = {15,19,23};
-    map[16] = {16,19,24};
-    map[17] = {17,25};
-    map[18] = {20,26,29};
-    map[19] = {21,26,30};
-    map[20] = {22,27,31};
-    map[21] = {23,27,32};
-    map[22] = {24,28,33};
-    map[23] = {25,28,34};
-    map[24] = {29,37};
-    map[25] = {30,35,38};
+    map[0] = {0, 1};
+    map[1] = {0, 2};
+    map[2] = {3, 5};
+    map[3] = {1, 3, 6};
+    map[4] = {2, 4, 7};
+    map[5] = {4, 8};
+    map[6] = {9, 12};
+    map[7] = {5, 9, 13};
+    map[8] = {6, 10, 14};
+    map[9] = {7, 10, 15};
+    map[10] = {8, 11, 16};
+    map[11] = {11, 17};
+    map[12] = {12, 20};
+    map[13] = {13, 18, 21};
+    map[14] = {14, 18, 22};
+    map[15] = {15, 19, 23};
+    map[16] = {16, 19, 24};
+    map[17] = {17, 25};
+    map[18] = {20, 26, 29};
+    map[19] = {21, 26, 30};
+    map[20] = {22, 27, 31};
+    map[21] = {23, 27, 32};
+    map[22] = {24, 28, 33};
+    map[23] = {25, 28, 34};
+    map[24] = {29, 37};
+    map[25] = {30, 35, 38};
     map[26] = {31, 35, 39};
     map[27] = {32, 36, 40};
-    map[28] = {33,36,41};
-    map[29] = {34,42};
-    map[30] = {37,43,46};
-    map[31] = {38,43,47};
-    map[32] = {39,44,48};
+    map[28] = {33, 36, 41};
+    map[29] = {34, 42};
+    map[30] = {37, 43, 46};
+    map[31] = {38, 43, 47};
+    map[32] = {39, 44, 48};
     map[33] = {40, 44, 49};
-    map[34] = {41,45,50};
-    map[35] = {42,45,51};
+    map[34] = {41, 45, 50};
+    map[35] = {42, 45, 51};
     map[36] = {46, 54};
-    map[37] = {47,52,55};
-    map[38] = {48,52,56};
+    map[37] = {47, 52, 55};
+    map[38] = {48, 52, 56};
     map[39] = {49, 53, 57};
     map[40] = {50, 53, 58};
     map[41] = {51, 59};
-    map[42] = {54,60};
-    map[43] = {55,60,63};
-    map[44] = {56,61,64};
+    map[42] = {54, 60};
+    map[43] = {55, 60, 63};
+    map[44] = {56, 61, 64};
     map[45] = {57, 61, 65};
-    map[46] = {58,62,66};
+    map[46] = {58, 62, 66};
     map[47] = {59, 62};
-    map[48] = {63,67};
-    map[49] = {64,67};
-    map[50] = {68,70};
-    map[51] = {66,68};
-    map[52] = {69,71};
-    map[53] = {70,71};
+    map[48] = {63, 67};
+    map[49] = {64, 67, 69};
+    map[50] = {65, 68, 70};
+    map[51] = {66, 68};
+    map[52] = {69, 71};
+    map[53] = {70, 71};
 }
 
 void Grid::test_map() {
-    std::cout<<"Map edges to neighbouring nodes:"<<std::endl;
+    /*std::cout<<"Map edges to neighbouring nodes:"<<std::endl;
     for (auto & kv : edge_ends) {
         std::cout<<kv.first<<": "<<kv.second.first<<", "<<kv.second.second<<std::endl;
     }
@@ -152,9 +151,16 @@ void Grid::test_map() {
             std::cout<<x<<", ";
         }
         std::cout<<std::endl;
-    }
-}
+    }*/
 
+    for (int i = 0; i < max_node; i++) {
+        if (valid_building(Colour::Red, i))
+            std::cout << "R " << i << std::endl;
+        if (valid_building(Colour::Orange, i))
+            std::cout << "O " << i << std::endl;
+    }
+    this->print_grid();
+}
 
 Grid::Grid() : goose_tile{nullptr} {
     edge_colour = std::unordered_map<size_t, Colour>();
@@ -219,7 +225,7 @@ Grid::Grid(std::ifstream &f) : goose_tile{nullptr} {
     node_owner_init(node_owner);
     edge_ends_init(edge_ends);
     adjacent_edges_init(adjacent_edges);
-    
+
     /*for (size_t i = 0; i <= max_edge; i++) {
         edge_colour[i] = Colour::NoColour;
     }
@@ -498,11 +504,9 @@ void Grid::print_grid() const {
     std::cout << std::endl;
 }
 
-
-
 bool Grid::valid_upgrade(size_t node_id) {
     if (node_owner[node_id]->get_type() == Building_Type::NoBuilding ||
-    node_owner[node_id]->get_type() == Building_Type::Tower) return false;
+        node_owner[node_id]->get_type() == Building_Type::Tower) return false;
     return true;
 }
 
@@ -516,7 +520,7 @@ void Grid::build_building(Player *player, size_t node_id) {
 }
 
 void Grid::upgrade_building(size_t node_id) {
-    if (!node_owner[node_id]) return; // no building exists
+    if (!node_owner[node_id]) return;  // no building exists
     node_owner[node_id]->upgrade();
 }
 
@@ -537,14 +541,14 @@ Grid::~Grid() {
 }
 
 bool Grid::valid_building(Colour player, size_t node_id) {
-   /*
-    Rules: 
-    • A residence may not be built on a vertex that is adjacent to a vertex with an existing residence.
-    • It is either the beginning of the game, in which case a residence can be built on any vertex, or they have built a road that
-      is adjacent to the vertex.
+    /*
+     Rules:
+     • A residence may not be built on a vertex that is adjacent to a vertex with an existing residence.
+     • It is either the beginning of the game, in which case a residence can be built on any vertex, or they have built a road that
+       is adjacent to the vertex.
 
-     check if there is an edge of the same color and if that edge does not already connect to a building
-   */   
+      check if there is an edge of the same color and if that edge does not already connect to a building
+    */
     for (auto u : adjacent_edges[node_id]) {
         if (edge_colour[u] == player &&
             !(node_owner[edge_ends[u].first]) &&
@@ -556,21 +560,31 @@ bool Grid::valid_building(Colour player, size_t node_id) {
 }
 
 bool Grid::valid_road(Colour player, size_t edge_id) {
-    if (edge_colour[edge_id]!=Colour::NoColour)
-         return false;
+    if (edge_colour[edge_id] != Colour::NoColour)
+        return false;
 
-    //we require an adjacent road or vertex
+    // we require an adjacent road or vertex
 
-    //road case
+    // road case
     for (auto u : adjacent_edges[edge_ends[edge_id].first])
         if (edge_colour[u] == player)
             return true;
-    
+
     for (auto u : adjacent_edges[edge_ends[edge_id].second])
         if (edge_colour[u] == player)
             return true;
 
-    //vertex case
-    return (node_owner[edge_ends[edge_id].first]->get_Owner()->get_Colour() == player
-            || node_owner[edge_ends[edge_id].second]->get_Owner()->get_Colour() == player);
+    // vertex case
+    Building *end_point_1 = node_owner[edge_ends[edge_id].first];
+    Building *end_point_2 = node_owner[edge_ends[edge_id].first];
+
+    if (end_point_1)
+        if (end_point_1->get_Owner()->get_Colour() == player)
+            return true;
+
+    if (end_point_2)
+        if (end_point_2->get_Owner()->get_Colour() == player)
+            return true;
+
+    return false;
 }
